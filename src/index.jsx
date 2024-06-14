f715137ff06678e5d083b65c5a1c80fb696057df
diyafatimaexetom <diya.fatima@exetom.com>
2024-06-14 17:30:26 +0500

    Latest Commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -12,9 +12,9 @@ AOS.init();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
